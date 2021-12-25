63d3b7145d5f30110a106f1005b757a95434b575
Greg Ingram <shaggygi97@gmail.com>
2021-12-25 13:58:13 -0500

    Used a few C#10 features

--- PcapNet/GlobalHeader.cs
+++ PcapNet/GlobalHeader.cs
@@ -1,28 +1,25 @@
-﻿using BinarySerialization;
+﻿namespace PcapNet;
 
-namespace PcapNet
+public class GlobalHeader
 {
-    public class GlobalHeader
-    {
-        [FieldOrder(0)]
-        public uint MagicNumber { get; set; }
+    [FieldOrder(0)]
+    public uint MagicNumber { get; set; }
 
-        [FieldOrder(1)]
-        public ushort VersionMajor { get; set; }
+    [FieldOrder(1)]
+    public ushort VersionMajor { get; set; }
 
-        [FieldOrder(2)]
-        public ushort VersionMinor { get; set; }
-        
-        [FieldOrder(3)]
-        public int GmtOffset { get; set; }
+    [FieldOrder(2)]
+    public ushort VersionMinor { get; set; }
 
-        [FieldOrder(4)]
-        public uint TimestampAccuracy { get; set; }
+    [FieldOrder(3)]
+    public int GmtOffset { get; set; }
 
-        [FieldOrder(5)]
-        public uint Length { get; set; }
+    [FieldOrder(4)]
+    public uint TimestampAccuracy { get; set; }
 
-        [FieldOrder(6)]
-        public uint LinkType { get; set; }
-    }
+    [FieldOrder(5)]
+    public uint Length { get; set; }
+
+    [FieldOrder(6)]
+    public uint LinkType { get; set; }
 }
---
+++ PcapNet/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using BinarySerialization;
+global using System.Collections.Generic;
+global using System;
+global using System.IO;
+global using System.Threading.Tasks;
